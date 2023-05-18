3b87d74cda6ddc8ebfe1dac07bde0d35d188d769
Vivek Raja <vivek.r.raja@gmail.com>
2023-05-18 05:56:41 -0700

    add middleware

--- data_mapper/settings.py
+++ data_mapper/settings.py
@@ -170,7 +170,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
