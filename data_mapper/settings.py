da8a3197396c940326d8f667ddef58052ff8bb60
Vivek Raja <vivek.r.raja@gmail.com>
2023-05-18 05:57:34 -0700

    add middleware

--- data_mapper/settings.py
+++ data_mapper/settings.py
@@ -170,12 +170,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
