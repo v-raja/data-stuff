1974fd7434720a3addb68cda238da0e3b47873fd
Vivek Raja <vivek.r.raja@gmail.com>
2023-05-18 05:39:59 -0700

    include static

--- data_mapper/settings.py
+++ data_mapper/settings.py
@@ -169,6 +169,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
